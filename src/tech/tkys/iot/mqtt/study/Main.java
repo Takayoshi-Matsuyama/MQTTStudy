0cc07dd8aed7f1c427609009dce7e13814ce8c94
Takayoshi <tkys0515@gmail.com>
2018-03-25 06:00:45 +0900

    Added test code publishes a test message to local broker.

--- src/tech/tkys/iot/mqtt/study/Main.java
+++ src/tech/tkys/iot/mqtt/study/Main.java
@@ -11,7 +11,44 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("MQTT Study");
 
-        testPahoMqttClientPublish();
+        publishToLocalBroker();
+//        testPahoMqttClientPublish();
+    }
+
+    /**
+     * Publishes a test message to local MQTT broker (tcp://localhost:1883).
+     */
+    private static void publishToLocalBroker() {
+        String topic        = "MQTTTest";
+        String content      = "Message from MqttPublisher";
+        int qos             = 2;
+        String broker       = "tcp://localhost:1883";
+        String clientId     = "JavaSample";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            System.exit(0);
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
     }
 
     /**
