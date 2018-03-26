b9498e749d09df8c7ee6ffd3ca1402115c690794
Takayoshi <tkys0515@gmail.com>
2018-03-27 06:19:12 +0900

    Refactored and added comments.

--- src/tech/tkys/iot/mqtt/study/Main.java
+++ src/tech/tkys/iot/mqtt/study/Main.java
@@ -6,18 +6,47 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * The Main class of this application.
+ */
 public class Main {
 
+    /**
+     * Provides entry point of this application.
+     * @param args - The command line arguments.
+     */
     public static void main(String[] args) {
-	    System.out.println("MQTT Study");
+        int exitStatus = 0;
+        try {
+            executeMqttCommunication();
+        } catch (Exception e) {
+            e.printStackTrace();
+            exitStatus = 1;
+        } finally {
+            System.exit(exitStatus);
+        }
+    }
 
-	    String brokerURI = "tcp://localhost:1883";
-	    String topic = "MQTTTest";
-	    int qos = 2;    // Quality of Service (2: Exactly once delivery)
+    /**
+     * Executes MQTT communication.
+     */
+    private static void executeMqttCommunication() {
+        //        String brokerURI = "tcp://192.168.0.11:1883"; // Specifies an external broker.
+        String brokerURI = "tcp://localhost:1883";
+
+        String topic = "MQTTTest";
+        int qos = 2;    // Quality of Service (2: Exactly once delivery)
 
         try {
-            MqttClient subscriberClient = subscribeToLocalBroker(brokerURI, topic);
-            publishToLocalBroker(brokerURI, topic, "Hello from MQTT Publisher", qos);
+            MqttClient subscriberClient = subscribeToBroker(brokerURI, topic);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+            String messageString = String.format("Hello from MQTT Publisher: %s", sdf.format(new Date()));
+            publishToBroker(brokerURI, topic, messageString, qos);
+
             if (subscriberClient != null) {
                 subscriberClient.disconnect();
                 System.out.println("[MQTT Subscriber] Disconnected");
@@ -32,7 +61,14 @@ public class Main {
         }
     }
 
-    private static MqttClient subscribeToLocalBroker(String brokerURI, String topic) throws MqttException {
+    /**
+     * Subscribes to a MQTT broker.
+     * @param brokerURI - URI specifies a broker.
+     * @param topic     - MQTT topic to subscribe.
+     * @return          - MQTT subscriber client object.
+     * @throws MqttException
+     */
+    private static MqttClient subscribeToBroker(String brokerURI, String topic) throws MqttException {
         MqttClient client = new MqttClient(brokerURI, MqttClient.generateClientId());
         client.setCallback( new MyMqttCallback() );
         System.out.println("[MQTT Subscriber] Connecting to broker: " + brokerURI);
@@ -45,22 +81,30 @@ public class Main {
     }
 
     /**
-     * Publishes a test message to local MQTT broker.
+     * Publishes a test message to a MQTT broker.
+     * @param brokerURI     - URI specifies a broker.
+     * @param topic         - MQTT topic to publish.
+     * @param messageString - Message string to publish.
+     * @param qos           - Quality of Service (0: At most once delivery, 1: At least once delivery, 2: Exactly once delivery)
+     * @throws MqttException
      */
-    private static void publishToLocalBroker(String brokerURI, String topic, String messageString, int qos) throws MqttException {
+    private static void publishToBroker(String brokerURI, String topic, String messageString, int qos) throws MqttException {
         MemoryPersistence persistence = new MemoryPersistence();
-
         MqttClient publisherClient = new MqttClient(brokerURI, MqttClient.generateClientId(), persistence);
+
         MqttConnectOptions connOpts = new MqttConnectOptions();
         connOpts.setCleanSession(true);
+
         System.out.println("[MQTT Publisher] Connecting to broker: " + brokerURI);
         publisherClient.connect(connOpts);
         System.out.println("[MQTT Publisher] Connected");
+
         System.out.println("[MQTT Publisher] Publishing message: " + messageString);
         MqttMessage message = new MqttMessage(messageString.getBytes());
         message.setQos(qos);
         publisherClient.publish(topic, message);
         System.out.println("[MQTT Publisher] Message published");
+
         publisherClient.disconnect();
         System.out.println("[MQTT Publisher] Disconnected");
     }
@@ -75,21 +119,24 @@ public class Main {
         int qos             = 2;
         String broker       = "tcp://iot.eclipse.org:1883";
         String clientId     = "JavaSample";
-        MemoryPersistence persistence = new MemoryPersistence();
 
+        MemoryPersistence persistence = new MemoryPersistence();
         MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+
         MqttConnectOptions connOpts = new MqttConnectOptions();
         connOpts.setCleanSession(true);
+
         System.out.println("Connecting to broker: "+broker);
         sampleClient.connect(connOpts);
         System.out.println("Connected");
+
         System.out.println("Publishing message: "+content);
         MqttMessage message = new MqttMessage(content.getBytes());
         message.setQos(qos);
         sampleClient.publish(topic, message);
         System.out.println("Message published");
+
         sampleClient.disconnect();
         System.out.println("Disconnected");
-        System.exit(0);
     }
 }
